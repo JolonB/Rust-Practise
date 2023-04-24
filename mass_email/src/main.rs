15d62d4882d85d64dc1503c18be0cc6b74750b95
JolonB <jolon.behrent@gmail.com>
2023-04-24 21:04:54 +1200

    Update config directory

--- mass_email/src/main.rs
+++ mass_email/src/main.rs
@@ -1,16 +1,16 @@
-use std::fs;
 use rand::Rng;
+use std::fs;
 
-fn send_mail(address: &str, message: &str) {
-
-}
+fn send_mail(address: &str, message: &str) {}
 
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let file_path: &str = "src/data.json";
+    let mut file_path = std::env::current_dir().unwrap();
+    file_path.push("config");
+    file_path.push("data.json");
     let file_contents: String =
-        fs::read_to_string(file_path).expect(&format!("File {} not found", file_path));
+        fs::read_to_string(&file_path).expect(&format!("File {} not found", file_path.display()));
     let parsed_contents: json::JsonValue = json::parse(&file_contents).unwrap();
 
     for (email_address, messages) in parsed_contents.entries() {
