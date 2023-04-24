e14559a0e286bd21a156eb20ebf63ca4ef26d40d
JolonB <jolon.behrent@gmail.com>
2023-04-24 20:52:34 +1200

    Implement printing out random message

---
+++ mass_email/README.md
@@ -0,0 +1,3 @@
+# Mass Email
+
+Parse a json file, select a random response, and send the email.
--- mass_email/src/main.rs
+++ mass_email/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use rand::Rng;
+
+fn send_mail(address: &str, message: &str) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let file_path: &str = "src/data.json";
+    let file_contents: String =
+        fs::read_to_string(file_path).expect(&format!("File {} not found", file_path));
+    let parsed_contents: json::JsonValue = json::parse(&file_contents).unwrap();
+
+    for (email_address, messages) in parsed_contents.entries() {
+        let message_index = rng.gen_range(0..messages.len());
+        let chosen_message = &messages[message_index];
+        println!("to: {}\nchosen message: {}", email_address, chosen_message);
+    }
 }
